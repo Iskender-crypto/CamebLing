6c394a96a381fc0585a866e6974677aa6429f4a4
Iskender-crypto <77961856+Iskender-crypto@users.noreply.github.com>
2024-01-20 05:37:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+useHead({
+  title: 'Gambling'
+})
+</script>
+
+<template>
+  <NuxtLayout class="h-full">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style lang='scss'>
+@import 'App.scss';
+</style>
